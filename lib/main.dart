e6b9f7591cbe584e6a17e39c61f44141b2e079e9
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-04 00:12:46 +0900

    DefaultTabController 구성

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KkungFlix',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            children: [
+              Container(),
+              Container(),
+              Container(),
+              Container(),
+            ],
+            physics: NeverScrollableScrollPhysics(), //스크롤이안되게
+          ),
+        ),
+      ),
+    );
+  }
+}
