a62bf8de5e8a7b5904879152459c5b1841d9ebdd
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-08 17:50:39 +0900

    내가 찜한 콘텐츠, 재생, 정보 버튼 생성

--- lib/widget/carousel_slider.dart
+++ lib/widget/carousel_slider.dart
@@ -4,7 +4,7 @@ import 'package:netflix_clone/model/model_movie.dart';
 
 class CarouselImage extends StatefulWidget {
   final List<Movie> movies;
-  CarouselImage({this.movies});//생성자.이 위젯에 필요한 데이터를 받아올 수 있게해줌. 
+  CarouselImage({this.movies}); //생성자.이 위젯에 필요한 데이터를 받아올 수 있게해줌.
   _CarouselImageState createState() => _CarouselImageState();
 }
 
@@ -21,21 +21,24 @@ class _CarouselImageState extends State<CarouselImage> {
   void initState() {
     super.initState();
     movies = widget.movies;
-    images = (movies.map((movie) => Image.asset('./images/' + movie.poster))).toList();
+    images = (movies.map((movie) => Image.asset('./images/' + movie.poster)))
+        .toList();
     keywords = movies.map((e) => e.keyword).toList();
     likes = movies.map((e) => e.like).toList();
-    _currentKeyword= keywords[0];
+    _currentKeyword = keywords[0];
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       child: Column(
-        children:[
+        children: [
           Container(
+            //상단간격빈공간
             padding: EdgeInsets.all(20),
           ),
           CarouselSlider(
+            //사진
             items: images,
             options: CarouselOptions(
               onPageChanged: (index, reason) {
@@ -46,7 +49,82 @@ class _CarouselImageState extends State<CarouselImage> {
               },
             ),
           ),
-          Container(),
+          Container(
+            //장르 텍스트
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
+            child: Text(
+              _currentKeyword,
+              style: TextStyle(fontSize: 11),
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //내가찜한콘텐츠 버튼
+                Container(
+                  child: Column(
+                    children: [
+                      likes[_currentPage]
+                          ? IconButton(
+                              icon: Icon(Icons.check),
+                              onPressed: () {},
+                            )
+                          : IconButton(
+                              icon: Icon(Icons.add),
+                              onPressed: () {},
+                            ),
+                      Text(
+                        '내가 찜한 콘텐츠',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                    ],
+                  ),
+                ),
+                //재생버튼
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.black,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(3),
+                        ),
+                        Text(
+                          '재생',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //정보버튼
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Column(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.info),
+                        onPressed: () {},
+                      ),
+                      Text(
+                        '정보',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
