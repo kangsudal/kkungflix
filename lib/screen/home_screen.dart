e94f1f838958b0047e5fffabd30c552cf4fdba3f
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-08 12:24:19 +0900

    2. 홈 화면에 상단 바 추가하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -17,12 +17,34 @@ class TopBar extends StatelessWidget {
     return Container(
       padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Image.asset(
             'images/logo-netflix.png',
             fit: BoxFit.contain,
             height: 45,
           ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
+            ),
+          )
         ],
       ),
     );
