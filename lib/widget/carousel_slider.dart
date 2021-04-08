04bf3f9bfa96bea680454ea8631d9288ae6239cb
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-08 16:11:58 +0900

    CarouselImage 위젯에서 관리할 변수와 초기값 선언

---
+++ lib/widget/carousel_slider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/model/model_movie.dart';
+
+class CarouselImage extends StatefulWidget {
+  final List<Movie> movies;
+  CarouselImage({this.movies});//생성자.이 위젯에 필요한 데이터를 받아올 수 있게해줌. 
+  _CarouselImageState createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  //state로 관리해줄변수들
+  List<Movie> movies;
+  List<Widget> images;
+  List<String> keywords;
+  List<bool> likes;
+  int _currentPage = 0;
+  String _currentKeyword;
+
+  @override
+  void initState() {
+    super.initState();
+    movies = widget.movies;
+    images = (movies.map((movie) => Image.asset('./images/' + movie.poster))).toList();
+    keywords = movies.map((e) => e.keyword).toList();
+    likes = movies.map((e) => e.like).toList();
+    _currentKeyword= keywords[0];
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
