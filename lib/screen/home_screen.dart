d3fd87a7111f0e1be333f61ff56bec90c869a559
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-08 11:25:32 +0900

    resize logo size

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return TopBar();
@@ -18,7 +18,11 @@ class TopBar extends StatelessWidget {
       padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
       child: Row(
         children: [
-          Image.asset('images/logo-netflix.png'),
+          Image.asset(
+            'images/logo-netflix.png',
+            fit: BoxFit.contain,
+            height: 45,
+          ),
         ],
       ),
     );
