673263601150e3ee1499cfffed8647a7281111b8
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-08 16:35:40 +0900

    HomeScreen 화면에 CarouselImage 위젯 출력

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/model/model_movie.dart';
+import 'package:netflix_clone/widget/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -10,19 +11,28 @@ class _HomeScreenState extends State<HomeScreen> {
     Movie.fromMap({
       'title':'나의 가족 나의 도시',
       'keyword': '가족/코미디/드라마',
-      'poster': 'poster_myfamily.png',
+      'poster': 'poster_myfamily.jpg',
       'like':false,
     }),
     Movie.fromMap({
       'title':'사랑의 불시착',
       'keyword': '사랑/로맨스/판타지',
-      'poster': 'poster_love.png',
+      'poster': 'poster_love.jpg',
       'like':false,
     }),
   ];
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(
+      children: [
+        Stack(
+          children: [
+            CarouselImage(movies:movies),
+            TopBar(),
+          ],
+        ),
+      ],
+    );
   }
 }
 
