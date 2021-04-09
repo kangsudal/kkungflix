4f557ff3864b67b940892969ea4719487754086d
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-09 20:21:14 +0900

    5. 홈 화면에 원형 이미지 / 사각형 이미지 슬라이더 위젯 추가하기 완료

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/model/model_movie.dart';
+import 'package:netflix_clone/widget/box_slider.dart';
 import 'package:netflix_clone/widget/carousel_slider.dart';
 import 'package:netflix_clone/widget/circle_slider.dart';
 
@@ -10,40 +11,40 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<Movie> movies = [
     Movie.fromMap({
-      'title':'나의 가족 나의 도시',
+      'title': '나의 가족 나의 도시',
       'keyword': '가족/코미디/드라마',
       'poster': 'poster_myfamily.jpg',
-      'like':false,
+      'like': false,
     }),
     Movie.fromMap({
-      'title':'사랑의 불시착',
+      'title': '사랑의 불시착',
       'keyword': '사랑/로맨스/판타지',
       'poster': 'poster_love.jpg',
-      'like':false,
+      'like': false,
     }),
     Movie.fromMap({
-      'title':'뷰티풀마인드',
+      'title': '뷰티풀마인드',
       'keyword': '드라마/로맨스',
       'poster': 'a-beautiful-mind.jpg',
-      'like':false,
+      'like': false,
     }),
     Movie.fromMap({
-      'title':'굿윌헌팅',
+      'title': '굿윌헌팅',
       'keyword': '드라마/로맨스',
       'poster': 'good-will-hunting.png',
-      'like':false,
+      'like': false,
     }),
     Movie.fromMap({
-      'title':'Life as a house',
+      'title': 'Life as a house',
       'keyword': '드라마/패밀리',
       'poster': 'Life-as-a-house.jpg',
-      'like':false,
+      'like': false,
     }),
     Movie.fromMap({
-      'title':'메멘토',
+      'title': '메멘토',
       'keyword': '스릴러/미스터리',
       'poster': 'memento.jpg',
-      'like':false,
+      'like': false,
     }),
   ];
   @override
@@ -52,11 +53,14 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         Stack(
           children: [
-            CarouselImage(movies:movies),
+            CarouselImage(movies: movies),
             TopBar(),
           ],
         ),
         CirclesSlider(
+          movies: movies,
+        ),
+        BoxSlider(
           movies: movies,
         ),
       ],
