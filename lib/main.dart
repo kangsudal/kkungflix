48b403cb134e31f3cd908cbbbe1bf219590ece99
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-14 19:37:24 +0900

    No Firebase App '[DEFAULT]' has been created - call Firebase App.initializeApp() 문제 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/screen/home_screen.dart';
 import 'package:netflix_clone/screen/more_screen.dart';
 import 'package:netflix_clone/widget/bottom_bar.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
