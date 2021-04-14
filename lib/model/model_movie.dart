044b0b69856230c8a1dda70170085a8c62dee4e4
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-14 15:06:12 +0900

    firestore로부터 데이터를 가져올 수 있게해주는 stream 생성

--- lib/model/model_movie.dart
+++ lib/model/model_movie.dart
@@ -1,15 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Movie {
   final String title;
   final String keyword;
   final String poster;
   final bool like;
+  final DocumentReference reference; //Firebase firestore에 데이터 컬럼을 참조할 수 있는 링크
 
-  Movie.fromMap(Map<String, dynamic> map)
-  : title = map['title'],
-  keyword = map['keyword'],
-  poster = map['poster'],
-  like = map['like'];
+  Movie.fromMap(Map<String, dynamic> map, {this.reference})
+      : title = map['title'],
+        keyword = map['keyword'],
+        poster = map['poster'],
+        like = map['like'];
 
+  Movie.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), reference: snapshot.reference);
+  //reference를 이용해 데이터에 대한 CRUD 기능을 처리할수있다
   @override
   String toString() => "Movie<$title:$keyword>";
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/model/model_movie.dart';
 import 'package:netflix_clone/widget/box_slider.dart';
@@ -9,44 +10,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Movie> movies = [
-    Movie.fromMap({
-      'title': '나의 가족 나의 도시',
-      'keyword': '가족/코미디/드라마',
-      'poster': 'poster_myfamily.jpg',
-      'like': false,
-    }),
-    Movie.fromMap({
-      'title': '사랑의 불시착',
-      'keyword': '사랑/로맨스/판타지',
-      'poster': 'poster_love.jpg',
-      'like': false,
-    }),
-    Movie.fromMap({
-      'title': '뷰티풀마인드',
-      'keyword': '드라마/로맨스',
-      'poster': 'a-beautiful-mind.jpg',
-      'like': false,
-    }),
-    Movie.fromMap({
-      'title': '굿윌헌팅',
-      'keyword': '드라마/로맨스',
-      'poster': 'good-will-hunting.png',
-      'like': false,
-    }),
-    Movie.fromMap({
-      'title': 'Life as a house',
-      'keyword': '드라마/패밀리',
-      'poster': 'Life-as-a-house.jpg',
-      'like': false,
-    }),
-    Movie.fromMap({
-      'title': '메멘토',
-      'keyword': '스릴러/미스터리',
-      'poster': 'memento.jpg',
-      'like': false,
-    }),
-  ];
+  // List<Movie> movies = [
+  //   Movie.fromMap({
+  //     'title': '나의 가족 나의 도시',
+  //     'keyword': '가족/코미디/드라마',
+  //     'poster': 'poster_myfamily.jpg',
+  //     'like': false,
+  //   }),
+  //   Movie.fromMap({
+  //     'title': '사랑의 불시착',
+  //     'keyword': '사랑/로맨스/판타지',
+  //     'poster': 'poster_love.jpg',
+  //     'like': false,
+  //   }),
+  //   Movie.fromMap({
+  //     'title': '뷰티풀마인드',
+  //     'keyword': '드라마/로맨스',
+  //     'poster': 'a-beautiful-mind.jpg',
+  //     'like': false,
+  //   }),
+  //   Movie.fromMap({
+  //     'title': '굿윌헌팅',
+  //     'keyword': '드라마/로맨스',
+  //     'poster': 'good-will-hunting.png',
+  //     'like': false,
+  //   }),
+  //   Movie.fromMap({
+  //     'title': 'Life as a house',
+  //     'keyword': '드라마/패밀리',
+  //     'poster': 'Life-as-a-house.jpg',
+  //     'like': false,
+  //   }),
+  //   Movie.fromMap({
+  //     'title': '메멘토',
+  //     'keyword': '스릴러/미스터리',
+  //     'poster': 'memento.jpg',
+  //     'like': false,
+  //   }),
+  // ];
+  // 더미데이터 대신 Firebase 콘솔에서 cloud firestore에 데이터입력해서 받아옴
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Stream<QuerySnapshot> streamData;
+
+  @override
+  void initState() {
+    streamData = firestore.collection('movie').snapshots();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
