41f46090f228cb7a80bd66ca2f47e386c1ae0fef
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-04-17 20:29:18 +0900

    검색화면 만들기, x버튼 만들기

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -8,19 +8,81 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   final TextEditingController _filter = TextEditingController(); //검색위젯 컨트롤
   FocusNode focusNode = FocusNode(); //커서 상태
-  String _searchText = ""; //현재 검색어 값 
-  
-  _SearchScreenState(){
+  String _searchText = ""; //현재 검색어 값
+
+  _SearchScreenState() {
     _filter.addListener(() {
       setState(() {
-        _searchText = _filter.text; //TextEditingController가 변화를 감지해, _searchText 상태를 변화시키는 코드
+        _searchText = _filter
+            .text; //TextEditingController가 변화를 감지해, _searchText 상태를 변화시키는 코드
       });
     });
   }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SafeArea(
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.black,
+              padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      focusNode: focusNode,
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                      autofocus: true,
+                      controller: _filter,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white12,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.white60,
+                          size: 20,
+                        ),
+                        suffixIcon: focusNode.hasFocus
+                            ? IconButton(
+                                icon: Icon(
+                                  Icons.cancel,
+                                  size: 20,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _filter.clear();
+                                    _searchText = "";
+                                  });
+                                },
+                              )
+                            : Container(),
+                        hintText: '검색',
+                        labelStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    flex: 3,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
